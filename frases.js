2dfe9ff3fa4776324f4f191b89ad4101ae9e7ccf
Guillermo González <ggonzalezh15@gmail.com>
2020-01-11 18:23:51 -0300

    BOT V0.1

---
+++ frases.js
@@ -0,0 +1,16 @@
+let frases = [
+    "Vasta",
+    "Impresentable",
+    "Txiuuuuuuuuuu",
+    "Esto no era funable en el 2016",
+    "Me voy mas funao que la mierda",
+    "Smitty qlo degenerado",
+    "CARTA TRAMPA NOMAAAS",
+    "CUM ON MY BODY AND IN MY EYE",
+    "PAN CARNE PAN",
+    "POR QUE BEEFEAN MIS CHALAS WEON",
+    "DE FREEE",
+    "DE GRATIS"
+];
+
+module.exports.frases = frases;
